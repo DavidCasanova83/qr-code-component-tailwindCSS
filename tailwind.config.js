a625a38a3730fd2491be81be8c76c048dc844160
david <dvd8301@ubuntu.lan>
2024-06-08 18:17:31 +0200

    first tailwind frontend mentor project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      keyframes: {
+        scrolling: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        scrolling: 'scrolling 30s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+}
+
